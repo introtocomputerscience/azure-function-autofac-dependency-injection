15b26bc613a3d37cdbdf98c5330d0b1eb5d92a09
Guillaume Guerra <guillaume.guerra@younited-credit.fr>
2020-02-13 16:08:57 +0100

    Added specific function to NetFramework example to validate injection of ILoggerFactory

---
+++ NetFrameworkExample/Functions/LoggerFunction.cs
@@ -0,0 +1,24 @@
+using System.Net;
+using System.Net.Http;
+using AutofacDIExample;
+using AutofacDIExample.Resolvers;
+using AzureFunctions.Autofac;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace AutofacDIExample.Functions
+{
+    [DependencyInjectionConfig(typeof(DIWithLoggerFactoryConfig))]
+    public class LoggerFunction
+    {
+        [FunctionName("LoggerFunction")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequestMessage request,
+            TraceWriter log,
+            [Inject]ILogWriter writer)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+            return request.CreateResponse(HttpStatusCode.OK, "Autofac managed to inject a ILogger<>");
+        }
+    }
+}
