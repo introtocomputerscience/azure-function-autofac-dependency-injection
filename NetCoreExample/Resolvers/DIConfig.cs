ea4945443b87dd1efb55e9763d4b23f504c92997
Brad Glass <brad.t.glass@gmail.com>
2020-04-01 22:39:01 +0100

    Include an example of ILogger<> injection in the .NET Core example
    This change only affects the example and shows how to
    enable logging for injected loggers, it does not change
    any functionality in the packaged version

--- NetCoreExample/Resolvers/DIConfig.cs
+++ NetCoreExample/Resolvers/DIConfig.cs
@@ -2,7 +2,9 @@
 using AutofacDIExample.Modules;
 using AzureFunctions.Autofac;
 using AzureFunctions.Autofac.Configuration;
+using AzureFunctions.Autofac.Shared.Extensions;
 using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.Logging;
 using NetCoreExample.Interfaces;
 using NetCoreExample.Models;
 using System;
@@ -11,12 +13,13 @@ namespace AutofacDIExample.Resolvers
 {
     public class DIConfig
     {
-        public DIConfig(string functionName, string baseDirectory)
+        public DIConfig(string functionName, string baseDirectory, ILoggerFactory factory)
         {
             DependencyInjection.Initialize(builder =>
             {
                 builder.RegisterModule(new TestModule());
                 builder.Register<DirectoryAnnouncer>(c => new DirectoryAnnouncer(baseDirectory)).As<IAnnouncer>();
+                builder.RegisterLoggerFactory(factory);
             }, functionName);
         }
     }
