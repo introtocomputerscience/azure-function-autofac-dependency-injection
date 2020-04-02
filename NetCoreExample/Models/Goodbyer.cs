d51ca2b0f5dca343634f8c2b811e09fe173502a0
Brad Glass <37139318+G18SSY@users.noreply.github.com>
2020-04-02 02:13:20 +0100

    Include an example of ILogger<> injection in the .NET Core example (#53)
    This change only affects the example and shows how to
    enable logging for injected loggers, it does not change
    any functionality in the packaged version

--- NetCoreExample/Models/Goodbyer.cs
+++ NetCoreExample/Models/Goodbyer.cs
@@ -1,9 +1,24 @@
-﻿namespace AutofacDIExample
+﻿using Microsoft.Extensions.Logging;
+
+namespace AutofacDIExample
 {
     public class Goodbyer : IGoodbyer
     {
+        private readonly ILogger<Goodbyer> logger;
+
+        public Goodbyer(ILogger<Goodbyer> logger)
+        {
+            this.logger = logger;
+        }
+
         public string Goodbye()
         {
+            // To see this in the Azure Function CLI window you need to provide a logging configuration in host.json 
+            //    that enables the warning level either for everything or for this class
+            //     For more information see 
+            //     https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1#configuration
+            logger.LogWarning("Saying goodbye");
+
             return "So long...";
         }
     }
